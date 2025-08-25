73b1f31e72809e8dfe63ff04fd9c94e701bf5ae5
Basil648 <basilshajan666@gmail.com>
2025-08-25 07:08:42 +0530

    learned about the reux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, reset } from './redux/CounterSlice'
+
+
+
 
 function App() {
+  const dispatch = useDispatch()
+  const count=useSelector((state)=>state.count)
   return (
     <>
-
+      <h1>redux toolkit counter</h1>
+      <h1>count:</h1>
+      <button onClick={()=>dispatch(increment)}>Increment</button>
+      <button onClick={()=>dispatch(decrement)}>Decrement</button>
+      <button onClick={()=>dispatch(reset)}>Reset</button>
     </>
   )
-  
+
 }
 
 export default App
